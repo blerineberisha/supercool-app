df0f958c36b0af66664fdabe49a774ea9888c981
blerineberisha <69892200+blerineberisha@users.noreply.github.com>
2021-11-25 11:12:50 +0100

    application

---
+++ src/main/java/blerine/berisha/supercoolapp/SupercoolAppApplication.java
@@ -0,0 +1,13 @@
+package blerine.berisha.supercoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupercoolAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupercoolAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/blerine/berisha/supercoolapp/SupercoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package blerine.berisha.supercoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupercoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
